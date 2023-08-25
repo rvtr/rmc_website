15b1da52bbd908e7c735943c3b5f1be61552b450
Ian Skinner <56081713+IanSkinner1982@users.noreply.github.com>
2023-08-25 06:01:30 -0400

    When the gitignore is ignored!

--- _site/assets/js/main.js
+++
@@ -1,6 +0,0 @@
-window.addEventListener('load', setFooterDate, false);
-
-function setFooterDate() {
-    let footerDate = document.getElementById("footerDate");
-    footerDate.textContent = new Date().getFullYear().toString();
-}
